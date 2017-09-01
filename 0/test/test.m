03866474799fe482f7cd9a6730eae71414ff8164
Varun Shijo <varunshi@buffalo.edu>
2017-09-01 13:44:02 -0400

    cleaned up ROI stuff. Changed to 1d window to simplify indexing

--- 0/test/test.m
+++ 0/test/test.m
@@ -2,32 +2,50 @@ load('/Users/varunshijo/homework/CSE573/0/release/data/blue.mat')
 load('/Users/varunshijo/homework/CSE573/0/release/data/red.mat')
 load('/Users/varunshijo/homework/CSE573/0/release/data/green.mat')
 
-base_image = cat(3,red,green,blue)
+base_image = cat(3,red,green,blue);
 
-dimensions = size(base_image)
+dimensions = size(base_image);
 
-window_centre = ceil(dimensions/2)
+window_centre = ceil(dimensions/2);
 
-temp_red = red(window_centre(1)-15:1:window_centre(1)+15,window_centre(2)-15:1:window_centre(2)+15)
-temp_green = green(window_centre(1)-15:1:window_centre(1)+15,window_centre(2)-15:1:window_centre(2)+15)
-temp_blue = blue(window_centre(1)-15:1:window_centre(1)+15,window_centre(2)-15:1:window_centre(2)+15)
+%temp_red = red(window_centre(1)-15:1:window_centre(1)+15,window_centre(2)-15:1:window_centre(2)+15)
+%temp_green = green(window_centre(1)-15:1:window_centre(1)+15,window_centre(2)-15:1:window_centre(2)+15)
+%temp_blue = blue(window_centre(1)-15:1:window_centre(1)+15,window_centre(2)-15:1:window_centre(2)+15)
 
-window = cat(3,temp_red,temp_green,temp_blue)
+%window = cat(3,temp_red,temp_green,temp_blue)
+window = imcrop(base_image,[window_centre(1)-5,window_centre(2),10,0]);
 
-reds = repmat(temp_red,1,1,31)
-greens = repmat(temp_green,1,1,31)
-blues = repmat(temp_blue,1,1,31)
+temp_red = window(:,:,1);
+temp_green = window(:,:,2);
+temp_blue = window(:,:,3);
 
-for i = 1:1:31
-    reds(:,:,i) = circshift(reds(:,:,i),i)
-    greens(:,:,i) = circshift(greens(:,:,i),i)
-    blues(:,:,i) = circshift(blues(:,:,i),i)
+reds = [];
+greens= [];
+blues = [];
+for i = 1:1:30
+    reds = [reds;circshift(temp_red,i)];
+    greens = [greens;circshift(temp_green,i)];
+    blues = [blues;circshift(temp_blue,i)];
 end
+%reds = repmat(temp_red,1,1,31)
+%greens = repmat(temp_green,1,1,31)
+%blues = repmat(temp_blue,1,1,31)
 
+% for i = 1:1:31
+%     reds(:,:,i) = circshift(reds(:,:,i),i)
+%     greens(:,:,i) = circshift(greens(:,:,i),i)
+%     blues(:,:,i) = circshift(blues(:,:,i),i)
+% end
+% 
+% 
+% corr_1 = []
+% for i = 1:1:31
+%     for j = 1:1:31
+%         corr_1 = cat(3,corr_1,normxcorr2(reds(:,:,i),greens(:,:,j)))
+%     end
+% end
 
-corr_1 = []
-for i = 1:1:31
-    for j = 1:1:31
-        corr_1 = cat(3,corr_1,normxcorr2(reds(:,:,i),greens(:,:,j)))
-    end
-end
+corr_rg = normxcorr2(reds,greens);
+corr_gb = normxcorr2(greens,blues);
+[foo,bar] = max(corr_rg)
+[ham,eggs] = max(foo)
