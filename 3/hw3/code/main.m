607bffb244fc5c474ae7179d5d17ff4360af8a5d
Varun Shijo <varunshi@buffalo.edu>
2017-11-11 17:17:01 -0500

    doing ransac looping in the function and messing up

--- 3/hw3/code/main.m
+++ 3/hw3/code/main.m
@@ -7,6 +7,7 @@ radius = 3;
 disp = 0;
 neighborhood_factor = 11; % center pixel will be factor'th position in the square 
 match_count = 200;
+epochs = 5;
 
 %harris for points of interest
 [left_cim,left_r,left_c] = harris(left,sigma,thresh,radius,disp);
@@ -20,13 +21,6 @@ padded_right = padarray(right,[neighborhood_factor,neighborhood_factor],'replica
 left_descriptors = neighborhood(padded_left,left_r,left_c,neighborhood_factor);
 right_descriptors = neighborhood(padded_right,right_r,right_c,neighborhood_factor);
 
-% left_ind = sub2ind(size(padded_left),padded_left);
-% right_ind = sub2ind(size(padded_right),padded_right);
-% 
-% %flattening neighborhoods to form descriptors
-% left_vectors = left_descriptors;
-% right_vectors = right_descriptors;
-
 %calculating distance
 distances = dist2(left_descriptors,right_descriptors); %returned row index is left index
 
@@ -37,10 +31,6 @@ match_indices = dist_indices(1:match_count);
 %indices of closest pairs in distance matrix
 [match_r,match_c] = ind2sub(size(distances),match_indices); %padded vector indices
 
-%looking up in descriptor vector
-% [left_match_r,left_match_c] = [left_r(match_r),left_c(match_r)];
-% [right_match_r,right_match_c] = [right_r(match_c),right_c(match_c)];
-
 left_r_main = left_r(match_r);
 left_c_main = left_c(match_r);
 right_r_main = right_r(match_c);
@@ -50,4 +40,4 @@ right_c_main = left_c(match_c);
 left_hom = [left_r_main,left_c_main,ones(match_count,1)];
 right_hom = [right_r_main,right_c_main,ones(match_count,1)];
 
-temp = ransac(left_hom,right_hom);
+temp = ransac(left_hom,right_hom,epochs);
--- 3/hw3/code/ransac.m
+++ 3/hw3/code/ransac.m
@@ -1,15 +1,26 @@
-function A = ransac(p1,p2)
-rand_indices = size(p1,1);
-p1 = p1(rand_indices,:);
-p2 = p2(rand_indices,:);
-A = [];
-for i = 1:size(p1,1)
-    A = cat(1,A,[-p1(i,:),zeros(1,3),p2(i,1)*p1(i,:);zeros(1,3),-p1(i,:),p2(i,2)*p1(i,:)]);
+function H = ransac(p1,p2,epochs)
+rows = size(p1,1);
+for loop = 1:epochs
+    rand_indices = transpose(randperm(rows,4))
+    p1_sel = p1(rand_indices,:);
+    p2_sel = p2(rand_indices,:);
+    A = [];
+    for i = 1:size(p1_sel,1)
+        A = cat(1,A,[-p1_sel(i,:),zeros(1,3),p2_sel(i,1)*p1_sel(i,:);zeros(1,3),-p1_sel(i,:),p2_sel(i,2)*p1_sel(i,:)]);
+    end
+    [u,s,v]=svd(A);
+    h = v(:,size(v,2));
+    A = reshape(h,[3,3]);
+    A = A./A(3,3);
+    H = A';
+    
+    projections = [];
+    for point_count = 1:size(p1_sel,1)
+        projected = H * [(p1_sel(point_count,:))'];
+        projected = projected./projected(end,1);
+        projected = [projected(1),projected(2)];
+        projections = cat(1,projections,projected);
+    end
+    projections;
 end
-[u,s,v]=svd(A);
-size(v)
-h = v(:,size(v,2));
-A = reshape(h,[3,3]);
-A = A./A(3,3);
-A = A';
 end
